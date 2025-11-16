87bc80769fce94f09fce5ed8b7270849ae838a3e
allindiailks <allindiasilks@gmail.com>
2025-11-16 18:36:28 +0530

    firstcommit

---
+++ src/main/java/com/webpagetrading/WebpagetradingApplication.java
@@ -0,0 +1,13 @@
+package com.webpagetrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpagetradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpagetradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webpagetrading/WebpagetradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webpagetrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpagetradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
